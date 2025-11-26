e8d66ed124c50fdffd73539d8fcb696cb9a82dbf
Duralax <73173215+Duralax@users.noreply.github.com>
2025-11-26 22:07:23 +0500

    lb2 + timus_1607 timus_1785 timus_1820

--- src/lb2/Example5.java
+++ src/lb2/Example5.java
@@ -2,6 +2,10 @@ package lb2;
 
 import java.util.Scanner;
 
+//Напишите программу, которая проверяет, сколько тысяч во введенном
+//пользователем числе (определяется четвертая цифра справа в десятичном
+//представлении числа).
+
 public class Example5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
